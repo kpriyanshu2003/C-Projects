0e86eed3920c26faa049ff7957d85e5e444ccab9
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-03-17 12:33:01 +0530

    Differentiated max_3.c program. (Two methods)

--- custom/data/max_3.c
+++ custom/data/max_3.c
@@ -6,11 +6,20 @@ int main()
 	int a, b, c, max;
 	printf("Enter three numbers : ");
 	scanf("%d %d %d", &a, &b, &c);
-	max = a;
-	if (b > max)
-		max = b;
-	if (c > max)
-		max = c;
+	if (a > b)
+	{
+		if (a > c)
+			max = a;
+		else
+			max = c;
+	}
+	else
+	{
+		if (b > c)
+			max = b;
+		else
+			max = c;
+	}
 	printf("Largest number is %d", max);
 	return 0;
 }
