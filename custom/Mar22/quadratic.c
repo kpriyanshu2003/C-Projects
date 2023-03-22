2e26287b4a2438ba20ef358e7d95f43af8795ffc
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-03-22 13:27:56 +0530

    Updated quadratic.c

--- custom/Mar22/quadratic.c
+++ custom/Mar22/quadratic.c
@@ -1,29 +1,33 @@
 // WAP to find the roots of a quadratic equation ax2 + bx + c = 0 using if else statements
 #include <stdio.h>
 #include <math.h>
-int main(){
+int main()
+{
 	float a, b, c, d, r, x1, x2;
-	printf("Enter the values for a, ,b ,c : ");
-	scanf("%f %f %f",&a,&b,&c);
-	d = b* b - 4 * a * c;
-	if( d == 0){
+	printf("Enter the values for a, b ,c : ");
+	scanf("%f %f %f", &a, &b, &c);
+	d = b * b - 4 * a * c;
+	if (d == 0)
+	{
 		printf("The roots are real and equal\n");
-		x1 = x2 = -b/(2*a);
-		printf("The roots are :\n %.2f and %.2f",x1, x2);
+		x1 = x2 = -b / (2 * a);
+		printf("The roots are :\n %.2f and %.2f", x1, x2);
 	}
-	else if ( d > 0 ){
+	else if (d > 0)
+	{
 		printf("The roots are real and unequal\n");
-		x1 = (-b + sqrt(d)) / ( 2 * a);
-		x2 = (-b - sqrt(d)) / ( 2 * a);
-		printf("The roots are :\n %.2f and %.2f",x1, x2);
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+		printf("The roots are :\n %.2f and %.2f", x1, x2);
 	}
-	else{
+	else
+	{
 		printf("The roots are imaginary\n");
-		r = - b / ( 2 * a);
+		x1 = -b / (2 * a);
 		d = -d;
-		d = sqrt(d)/(2*a);
-		printf("The roots are :\n %.2f + i%.2f",r, d);
-		printf("The roots are :\n %.2f - i%.2f",r, d);
+		x2 = sqrt(d) / (2 * a);
+		printf("The roots are :\n %.2f + i%.2f", x1, x2);
+		printf(" and %.2f - i%.2f", x1, x2);
 	}
 	return 0;
 }
