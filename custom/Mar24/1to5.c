9b9b737948fb64565243467e17e9fec802ac7f39
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-03-24 12:14:11 +0530

    Created mul_table.c

--- custom/Mar24/1to5.c
+++ custom/Mar24/1to5.c
@@ -1,10 +1,12 @@
 // WAP to print all numbers between 1 to 5
 #include <stdio.h>
-int main(){
+int main()
+{
 	int i = 1;
-	while( i <= 5){
-	printf("%d ",i);
-	i++;
+	while (i <= 5)
+	{
+		printf("%d ", i);
+		i++;
 	}
 	return 0;
-	}
+}
