3f825d48e6838f9d378031e35b6976ed828be309
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-03-29 11:48:44 +0530

    Updated sumOdigit.c

--- custom/Mar29/sumOdigit.c
+++ custom/Mar29/sumOdigit.c
@@ -6,11 +6,10 @@ int main()
     int n, d, s = 0;
     printf("Enter a number : ");
     scanf("%d", &n);
-    for (; n != 0;)
+    for (; n != 0; n /= 10)
     {
         d = n % 10;
         s += d;
-        n = n / 10;
     }
     printf("The sum of digits is : %d", s);
     return 0;
