7787338a438862bb3187039d9a6138abfd698dcf
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-03-31 12:05:22 +0530

    Created sumOdigit_1.c

---
+++ data/sumOdigit_1.c
@@ -0,0 +1,24 @@
+// WAP to find the sum of digits and the sum of its result until it is a single digit number
+#include <stdio.h>
+int main()
+{
+    int n, d, s = 0;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    while (n != 0)
+    {
+        d = d % 10;
+        s = s + d;
+        n = n / 10;
+    }
+
+    while (s >= 10)
+    {
+        int s = 0;
+        d = s % 10;
+        s = s + d;
+    }
+
+    printf("%d", s);
+    return 0;
+}
