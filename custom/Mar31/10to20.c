e23852b4273d95ff55d9834a919b0aa6c4470350
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-04-12 11:38:23 +0530

    Fixed missing files

---
+++ custom/Mar31/10to20.c
@@ -0,0 +1,12 @@
+// WAP using do while loop to print numbers 10 to 20
+#include <stdio.h>
+int main()
+{
+    int i = 10;
+    do
+    {
+        printf("%d ", i);
+        i++;
+    } while (i <= 20);
+    return 0;
+}
