183faad995eac15649e7ff6bf376caa922355d59
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-05-12 10:38:27 +0530

    Updated to latest

--- custom/May10/strrev.c
+++ custom/May10/strrev.c
@@ -4,8 +4,10 @@
 int main()
 {
     char s[50];
-    printf("Enter a string ");
+    printf("Enter a string : ");
     scanf("%s", s);
-    printf("Reversed String : %s", strrev(s));
+    // printf("Reversed String : %s", strrev(s));
+    for(int i =strlen(s)-1;i>=0;i--)
+    printf("%c",s[i]);
     return 0;
 }
