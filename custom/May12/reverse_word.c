2013365d9bd0e3d5c6a10c9f7b027e43571e6db0
Priyanshu Kumar <az.priyanshu.rockstar@gmail.com>
2023-05-14 16:52:43 +0530

    Fixed minor mistakes

--- custom/May12/reverse_word.c
+++ custom/May12/reverse_word.c
@@ -1,19 +1,19 @@
 // WAP to input a string and reverse each word in its place
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 int main()
 {
-    char str[100], rev[100];
+    char str[100];
     int c = 0;
     printf("Enter a string : ");
     fgets(str, 100, stdin);
     for (int i = 0; i < strlen(str); i++)
         if (str[i] == ' ' || str[i] == '\n')
             c++;
+    printf("The final string : ");
     for (int n = 1; n <= c; n++)
     {
-        char word[100] = {};
+        char word[42] = {0}; // as the longest word is 42 characters long
         int count = 0, i = 0, j = 0;
         while (str[i] != '\0')
         {
@@ -34,7 +34,6 @@ int main()
             }
             i++;
         }
-        // reverse word
         int cond = 0;
         for (int k = strlen(word) - 1; k >= 0; k--)
             printf("%c", word[k]);
